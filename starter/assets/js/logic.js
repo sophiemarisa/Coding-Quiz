d28db4ed7d2fea34c8cfbe3246e56b63ebc1c551
sophiemarisa <sophiecharlwood@live.co.uk>
2023-01-17 12:03:14 +0000

    select answers

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -23,84 +23,204 @@ function startQuiz() {
 
 function setNextQuestion() {
     showQuestion(questionsArray);
+  
 }
+
 function showQuestion(question) {
-    questionTitleElement.innerText = question[currentQuestionIndex].question;
-    var correctIndex = 0;
-    var answers = [];
-    for (i = 0; i < questionsArray[currentQuestionIndex].answer.length; i++) {
 
-        if(questionsArray[0].answer[i].correct === true){
-            correctIndex = i;
-        }
-        
-        answers.push(questionsArray[0].answer[i].text);
+
+    if(currentQuestionIndex > question.length){
+        endQuiz
     }
+    else{
 
-    ans1.innerText = answers[0].toString();
-    ans2.innerText = answers[1].toString();
-    ans3.innerText = answers[2].toString();
-    ans4.innerText = answers[3].toString();
+        questionTitleElement.innerText = question[currentQuestionIndex].question;
+        var correctIndex = 0;
+        var answers = [];
+        var correctAns = "";
+        for (i = 0; i < questionsArray[currentQuestionIndex].answer.length; i++) {
 
-    correctIndex = correctIndex + 1;
-
-    if(correctIndex = 1){
-        ans1.dataset.correct = true;
-    }
-    else if(correctIndex = 2){
-        ans2.dataset.correct = true;
-    }
-    else if(correctIndex = 3){
-        ans3.dataset.correct = true;
-    }
-    else if(correctIndex = 4){
-        ans4.dataset.correct = true;
-    }
-
-    console.log(ans1.dataset.correct);
-    console.log(ans2.dataset.correct);
-    console.log(ans3.dataset.correct);
-    console.log(ans4.dataset.correct);
-
-    for(let i=0; i<ansButtons.length; i++){
-
-        ansButtons[i].addEventListener("click", function(event){
-            event.preventDefault();
-            var isCorrect = ansButtons[i].dataset.correct;
-            if(isCorrect = true){
-                score = score + 10;
-                console.log(score);
+            if (questionsArray[currentQuestionIndex].answer[i].correct === true) {
+                correctIndex = i;
+                correctAns = questionsArray[0].answer[i].text;
             }
-        });
+
+            answers.push(questionsArray[currentQuestionIndex].answer[i].text);
+        }
+
+
+        ans1.innerText = answers[0].toString();
+        ans2.innerText = answers[1].toString();
+        ans3.innerText = answers[2].toString();
+        ans4.innerText = answers[3].toString();
+
+        correctIndex = correctIndex + 1;
+
+        if (correctIndex = 1) {
+            ans1.dataset.correct = true;
+        }
+        else if (correctIndex = 2) {
+            ans2.dataset.correct = true;
+        }
+        else if (correctIndex = 3) {
+            ans3.dataset.correct = true;
+        }
+        else if (correctIndex = 4) {
+            ans4.dataset.correct = true;
+        }
+
+        selectAnswer();
+
+    }
         
-    }
-
-    //Function To Render My Answers// 
-    function showAnswer(questionsArray) {
-        //console.log(questionsArray)
-    }
-    //showAnswer(questionsArray)
-}
-
-
-function showAnswer(question, correctIndex){
-
-}
+    };
 
 function selectAnswer() {
+    ansButtons[0].addEventListener("click", function (event) {
+        event.preventDefault();
+        var isCorrect = ansButtons[0].dataset.correct;
+        if (isCorrect === "true") {
+            score = score + 10;
+            this.removeEventListener("click", arguments.callee,false);
+            
+        }
+        else {
+            this.removeEventListener("click", arguments.callee,false);
+            
+        };
+
+
+      
+
+        currentQuestionIndex = currentQuestionIndex + 1;
+
+
+        showQuestion(questionsArray);
+    });
+
+    ansButtons[1].addEventListener("click", function (event) {
+        event.preventDefault();
+        var isCorrect = ansButtons[1].dataset.correct;
+        if (isCorrect === "true") {
+            score = score + 10;
+            this.removeEventListener("click", arguments.callee,false);
+            
+        }
+        else {
+            this.removeEventListener("click", arguments.callee,false);
+            
+        };
+
+
+      
+
+        currentQuestionIndex = currentQuestionIndex + 1;
+
+
+
+        showQuestion(questionsArray);
+    });
+
+    ansButtons[2].addEventListener("click", function (event) {
+        event.preventDefault();
+        var isCorrect = ansButtons[2].dataset.correct;
+        if (isCorrect === "true") {
+            score = score + 10;
+            this.removeEventListener("click", arguments.callee,false);
+            
+        }
+        else {
+            this.removeEventListener("click", arguments.callee,false);
+            
+        };
+
+        currentQuestionIndex = currentQuestionIndex + 1;
+
+        showQuestion(questionsArray);
+    });
+
+    ansButtons[3].addEventListener("click", function (event) {
+        event.preventDefault();
+        var isCorrect = ansButtons[3].dataset.correct;
+        if (isCorrect === "true") {
+            score = score + 10;
+            this.removeEventListener("click", arguments.callee,false);
+            
+        }
+        else {
+            this.removeEventListener("click", arguments.callee,false);
+            
+        };
+
+
+
+        currentQuestionIndex = currentQuestionIndex + 1;
+
+        showQuestion(questionsArray);
+    });
 
 }
+
+function endQuiz(){
+    console.log("end")
+};
 
 var questionsArray = [
-{
-    question: "Fav sport",
-    answer: [
-        { text: 'Football', correct: true },
-        { text: "Golf", correct: false },
-        { text: "BasketBall", correct: false },
-        { text: "Rugby", correct: false }
-    ]
-}
+    {
+        question: "Fav sport",
+        answer: [
+            { text: 'Football', correct: true },
+            { text: "Golf", correct: false },
+            { text: "BasketBall", correct: false },
+            { text: "Rugby", correct: false }
+        ]
+    },
+
+    {
+        question: "2+2=?",
+        answer: [
+            { text: '3', correct: false },
+            { text: '5', correct: false },
+            { text: '4', correct: true },
+            { text: '6', correct: false },
+
+        ]
+    },
+
+    {
+        question: "3+2=?",
+        answer: [
+            { text: '3', correct: false },
+            { text: '5', correct: true },
+            { text: '4', correct: false },
+            { text: '6', correct: false },
+
+        ]
+    },
+
+    {
+        question: "4+2=?",
+        answer: [
+            { text: '3', correct: false },
+            { text: '5', correct: false },
+            { text: '4', correct: false },
+            { text: '6', correct: true },
+
+        ]
+    },
+
+    {
+        question: "5-2=?",
+        answer: [
+            { text: '3', correct: true },
+            { text: '5', correct: false },
+            { text: '4', correct: false },
+            { text: '6', correct: false },
+
+        ]
+    }
+
+
 
 
 ]
